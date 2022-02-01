cdf23273425c8ae3294b0ee084460d1f376c4bcf
Marcos Leite <m.abner@live.co.uk>
2022-02-01 01:08:36 -0300

    console.log cleanup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,16 +5,11 @@ var add = document.getElementById("add");
 
 function addTask() {
 	var li = document.createElement("li");
-	console.log(li);
 	var info = document.getElementById("taskName").value;
-	console.log(info);
 	taskInfo = document.createTextNode(info);
-	console.log(taskInfo);
 
 	taskList.appendChild(li);
-	console.log(taskList);
 	li.appendChild(taskInfo);
-	console.log(li);
 }
 
 add.addEventListener("click", addTask);
